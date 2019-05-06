028e1df835c1683a45ccab6bdf61fc19ea8d1c96
Jae Son <>
2019-05-06 14:30:59 -0400

    function and class component event handling

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+    <FunctionClick />
+    <ClassClick />
+    {/* <Counter /> */}
       {/* <Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman">
+      </Greet> */}
+      {/* <Greet name="Clark" heroName="Superman">
         <button>Action</button>
-      </Greet>
-      <Greet name="Diane" heroName="Wonderwoman"/>
-      <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Clark" heroName="Superman"/>
+      </Greet> */}
+      {/* <Greet name="Diane" heroName="Wonderwoman"/>
+      <Welcome name="Bruce" heroName="Batman"/> */}
+      {/* <Welcome name="Clark" heroName="Superman"/>
       <Welcome name="Diane" heroName="Wonderwoman"/> */}
-      <Message />
+      {/* <Message /> */}
     </div>
   );
 }
