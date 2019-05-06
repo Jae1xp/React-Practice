94a52e9db72c83ea9b9ec43b0d39928e0971ad69
Jae Son <>
2019-05-06 11:12:19 -0400

    react practice up to state tutorial

---
+++ src/components/Hello.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello Jae</h1>
+    </div>
+  )
+  //   return React.createElement(
+  //   'div', 
+  //   {id: 'hello',
+  //   className: 'dummyClass'
+  // }, 
+  //   React.createElement('h1', null, 'Hello Jae'))
+
+}
+
+export default Hello;
