6371155295b37a1bcca195794c8a306b2e3cd8bd
Jae Son <>
2019-05-07 17:37:01 -0400

    basics of form handling

--- src/App.js
+++ src/App.js
@@ -16,14 +16,16 @@ import StyleSheet from './components/StyleSheet';
 import Inline from './components/Inline';
 import './appStyles.css';
 import styles from './appStyles.module.css';
+import Form from './components/Form';
 
 
 function App() {
   return (
     <div className="App">
-    <h1 className='error'>Error</h1>
-    <h1 className={styles.success}>Success</h1>
-    <Inline />
+    <Form />
+    {/* <h1 className='error'>Error</h1>
+    <h1 className={styles.success}>Success</h1> */}
+    {/* <Inline /> */}
     {/* <StyleSheet primary={true} /> */}
     {/* <NameList /> */}
     {/* <UserGreeting /> */}
