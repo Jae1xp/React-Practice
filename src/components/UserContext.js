f786005874bf224ff0392e867ec295e1f0a00f41
Jae Son <>
2019-05-09 15:48:11 -0400

    context pt 3

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('Default value here') // only used when a component doesn't have a matching provider above it in the component tree
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
 export { UserProvider, UserConsumer }
+
+export default UserContext // being used to context type property
